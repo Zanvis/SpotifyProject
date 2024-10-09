67d1fea13dae400dadb31902effd8163ae1c5314
Antoni Piwowarski <legoo1312@gmail.com>
2024-10-09 15:19:58 +0200

    Changed cors

--- backend/server.js
+++ backend/server.js
@@ -13,11 +13,13 @@ const port = 3000;
 const uploadDir = 'uploads';
 
 // Middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
 app.use(express.static('public'));
-
+app.use(cors({
+    origin: ['https://spotify-project-ecru.vercel.app', 'localhost:4200']
+}));
 // MongoDB connection
 const uri = process.env.MONGODB_URI;
 
