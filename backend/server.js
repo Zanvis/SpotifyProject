0226b579c8c4758ab9a6411c5b07789c63f55544
Antoni Piwowarski <legoo1312@gmail.com>
2024-10-10 23:30:00 +0200

    Repaired cors

--- backend/server.js
+++ backend/server.js
@@ -19,8 +19,8 @@ if (!fs.existsSync(uploadDir)) {
 // Middleware
 // app.use(cors());
 app.use(cors({
-    // origin: ['https://soundsphere-project.vercel.app', 'http://localhost:4200'],
-    origin: ['https://soundsphere-project.vercel.app'],
+    origin: ['https://soundsphere-project.vercel.app', 'http://localhost:4200'],
+    // origin: ['https://soundsphere-project.vercel.app'],
     methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
